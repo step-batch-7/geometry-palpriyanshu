3baf7bd430cd2e5af55853c2c4dc215a0b0b8e1c
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2019-12-13 16:06:21 +0530

    modified names

--- src/line.js
+++ src/line.js
@@ -1,24 +1,26 @@
 const areEqualPoints = function(point1, point2) {
-  const abscissa = point1.x === point2.x;
-  const ordinate = point1.y === point2.y;
-  return abscissa && ordinate;
+  const areXCoordinatesEqual = point1.x == point2.x;
+  const areYCoordinatesEqual = point1.y == point2.y;
+  return areXCoordinatesEqual && areYCoordinatesEqual;
 };
 
 class Line {
   constructor(endA, endB) {
-    this.endA = { ...endA };
-    this.endB = { ...endB };
+    this.endA = { x: endA.x, y: endA.y };
+    this.endB = { x: endB.x, y: endB.y };
   }
 
   toString() {
     return `Line (${this.endA.x},${this.endA.y})-(${this.endB.x},${this.endB.y})`;
   }
 
-  isEqual(otherLine) {
+  isEqual(other) {
+    if (!(other instanceof Line)) {
+      return false;
+    }
     return (
-      otherLine instanceof Line &&
-      areEqualPoints(this.endA, otherLine.endA) &&
-      areEqualPoints(this.endB, otherLine.endB)
+      areEqualPoints(this.endA, other.endA) &&
+      areEqualPoints(this.endB, other.endB)
     );
   }
 }
