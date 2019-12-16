a82718de2269a944ea1f4d2557425975e557e224
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2019-12-16 14:04:37 +0530

    added test cases for isEqual Method for circle class

--- test/testCircle.js
+++ test/testCircle.js
@@ -15,5 +15,23 @@ describe("Circle", function() {
       const circle2 = new Circle({ x: 0, y: 0 }, 5);
       assert.ok(circle1.isEqual(circle2));
     });
+
+    it("should inValidate when centre of two circles is not equal", function() {
+      const circle1 = new Circle({ x: 0, y: 1 }, 5);
+      const circle2 = new Circle({ x: 0, y: 0 }, 5);
+      assert.notOk(circle1.isEqual(circle2));
+    });
+
+    it("should inValidate when radius of two circles is not equal", function() {
+      const circle1 = new Circle({ x: 0, y: 0 }, 3);
+      const circle2 = new Circle({ x: 0, y: 0 }, 5);
+      assert.notOk(circle1.isEqual(circle2));
+    });
+
+    it("should inValidate when object is not instance of Circle", function() {
+      const circle1 = new Circle({ x: 0, y: 0 }, 3);
+      const circle2 = { centre: { x: 0, y: 0 }, radius: 5 };
+      assert.notOk(circle1.isEqual(circle2));
+    });
   });
 });
