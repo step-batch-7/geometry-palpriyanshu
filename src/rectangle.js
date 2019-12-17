07862330feaa8eb796fac815891757c468ad23c6
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2019-12-17 14:12:55 +0530

    added a property area in Rectangle class

--- src/rectangle.js
+++ src/rectangle.js
@@ -10,6 +10,13 @@ class Rectangle {
   toString() {
     return `[Rectangle (${this.endA.x},${this.endA.y}) to (${this.endB.x},${this.endB.y})]`;
   }
+
+  get area() {
+    const point = new Point(this.endB.x, this.endA.y);
+    const length = this.endA.findDistanceTo(point);
+    const width = this.endB.findDistanceTo(point);
+    return length * width;
+  }
 }
 
 module.exports = Rectangle;
--- test/testRectangle.js
+++ test/testRectangle.js
@@ -2,9 +2,21 @@ const assert = require("chai").assert;
 const Rectangle = require("../src/rectangle.js");
 const Point = require("../src/point.js");
 
-describe("toString", function() {
-  it("should give string representation of rectangle", function() {
-    const rectangle = new Rectangle({ x: 0, y: 0 }, { x: 5, y: 5 });
-    assert.deepStrictEqual(rectangle.toString(), "[Rectangle (0,0) to (5,5)]");
+describe("Rectangle", function() {
+  describe("toString", function() {
+    it("should give string representation of rectangle", function() {
+      const rectangle = new Rectangle({ x: 0, y: 0 }, { x: 5, y: 5 });
+      assert.deepStrictEqual(
+        rectangle.toString(),
+        "[Rectangle (0,0) to (5,5)]"
+      );
+    });
+  });
+
+  describe("area", function() {
+    it("should give area of a rectangle", function() {
+      const rectangle = new Rectangle({ x: 0, y: 0 }, { x: 5, y: 5 });
+      assert.strictEqual(rectangle.area, 25);
+    });
   });
 });
