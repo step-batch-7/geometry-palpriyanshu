da20289420c03278d35fcfc971b3d95f4d83fbe0
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2019-12-18 08:25:34 +0530

    extracted a function getSides from rectangle class

--- src/rectangle.js
+++ src/rectangle.js
@@ -7,6 +7,16 @@ const isCoordinateInRange = function(coordinate, range) {
   return coordinate >= start && coordinate <= end;
 };
 
+const getSides = function(endA, endC) {
+  const endB = { x: endC.x, y: endA.y };
+  const endD = { x: endA.x, y: endC.y };
+  const AB = new Line(endA, endB);
+  const BC = new Line(endB, endC);
+  const CD = new Line(endC, endD);
+  const AD = new Line(endA, endD);
+  return [AB, BC, CD, AD];
+};
+
 class Rectangle {
   constructor(endA, endC) {
     this.endA = new Point(endA.x, endA.y);
@@ -19,25 +29,15 @@ class Rectangle {
     return `[Rectangle ${point1} to ${point2}]`;
   }
 
-  get sides() {
-    const endB = { x: this.endC.x, y: this.endA.y };
-    const endD = { x: this.endA.x, y: this.endC.y };
-    const AB = new Line(this.endA, endB);
-    const BC = new Line(endB, this.endC);
-    const CD = new Line(this.endC, endD);
-    const AD = new Line(this.endA, endD);
-    return [AB, BC, CD, AD];
-  }
-
   get area() {
-    const length = this.sides[0].length;
-    const width = this.sides[1].length;
+    const length = getSides(this.endA, this.endC)[0].length;
+    const width = getSides(this.endA, this.endC)[1].length;
     return length * width;
   }
 
   get perimeter() {
-    const length = this.sides[0].length;
-    const width = this.sides[1].length;
+    const length = getSides(this.endA, this.endC)[0].length;
+    const width = getSides(this.endA, this.endC)[0].length;
     return 2 * (length + width);
   }
 
@@ -48,7 +48,7 @@ class Rectangle {
 
   hasPoint(point) {
     if (!(point instanceof Point)) return false;
-    return this.sides.some(line => point.isOn(line));
+    return getSides(this.endA, this.endC).some(line => point.isOn(line));
   }
 
   covers(point) {
